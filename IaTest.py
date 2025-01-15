e3e7b1e6926cbf9d635dd62e01386de04fa97906
MushroomDevs-crypto <mushroomdevs@hotmail.com>
2025-01-15 16:45:43 -0300

    user

--- IaTest.py
+++ IaTest.py
@@ -12,6 +12,8 @@ consumer_key = os.getenv("TWITTER_API_KEY")
 consumer_secret = os.getenv("TWITTER_API_SECRET")
 access_token = os.getenv("TWITTER_ACCESS_TOKEN")
 access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+user = None
+user_id = None
 
 # Cria o cliente com a API v2
 client = tweepy.Client(
