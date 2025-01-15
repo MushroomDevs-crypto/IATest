20afeb2b2235fc553bdf3f547c58785562356b2a
MushroomDevs-crypto <mushroomdevs@hotmail.com>
2025-01-14 23:26:53 -0300

    test

--- IaTest.py
+++ IaTest.py
@@ -1,5 +1,6 @@
 import tweepy
 import os
+import sys
 
 # Set up API credentials from environment variables
 api_key = os.getenv("TWITTER_API_KEY")
@@ -8,20 +9,32 @@ access_token = os.getenv("TWITTER_ACCESS_TOKEN")
 access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
 
-# Set up Twitter client
-client = tweepy.Client(
-    bearer_token=bearer_token,
-    consumer_key=api_key,
-    consumer_secret=api_secret,
-    access_token=access_token,
-    access_token_secret=access_token_secret
-)
+# Validate credentials
+if not all([api_key, api_secret, access_token, access_token_secret, bearer_token]):
+    print("Error: Missing Twitter API credentials. Please check your environment variables.")
+    sys.exit(1)
 
-# Set up OAuth1 authentication for API v1.1
-auth = tweepy.OAuth1UserHandler(
-    api_key, api_secret, access_token, access_token_secret
-)
-api = tweepy.API(auth)
+try:
+    # Set up Twitter client
+    client = tweepy.Client(
+        bearer_token=bearer_token,
+        consumer_key=api_key,
+        consumer_secret=api_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
+        wait_on_rate_limit=True
+    )
+
+    # Verify credentials
+    client.get_me()
+except tweepy.errors.Unauthorized:
+    print("Error: Invalid credentials. Please check your Twitter API keys and tokens.")
+    print("Make sure you have created a project in the Twitter Developer Portal.")
+    sys.exit(1)
+except Exception as e:
+    print(f"Error during authentication: {e}")
+    sys.exit(1)
+
 
 class MyStreamListener(tweepy.StreamingClient):
     def on_tweet(self, tweet):
